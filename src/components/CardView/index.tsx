42d3c83939956d4de0acc2c53eb2c31783f0be37
Ednardo Gomes Dos Santos <ednardo.santos@lanlink.com.br>
2025-08-20 22:58:48 -0300

    feat: create RederCarDetails component

--- src/components/CardView/index.tsx
+++ src/components/CardView/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { View, Text, Button, Image } from 'react-native'
+import { View, Image, Text } from 'react-native'
 
 import { Divider } from '../Divider'
 
 import { styles } from './style'
 import Logo from '../../../assets/lamborghini-logo.png'
+import { CAR_ASSETS_BASE_URL } from '../../constants/car'
 
 export function CardView() {
   const renderLogoBox = () => (
@@ -13,10 +14,27 @@ export function CardView() {
     </View>
   )
 
+  const renderCarDetails = () => (
+    <View style={{ alignItems: 'center' }}>
+      <Text style={styles.carBrand}>Lamborghini</Text>
+
+      <Text style={styles.carName}>MODEL</Text>
+    </View>
+  )
+  
+  const renderCarImage = () => (
+    <Image style={styles.carImage} source={{ uri: `${CAR_ASSETS_BASE_URL}2.png` }} />
+  )
+
   return (
     <View style={styles.imageContainer}>
       {renderLogoBox()}
+
       <Divider />
+      
+      {renderCarDetails()}
+
+      {renderCarImage()}
     </View>
   )
 }
--- src/components/CardView/style.tsx
+++ src/components/CardView/style.tsx
@@ -19,5 +19,24 @@ export const styles = StyleSheet.create({
     flex: 1,
     width: '40%',
     resizeMode: 'contain'
+  },
+  carBrand: {
+    color: '#fff',
+    fontWeight: '400',
+    fontSize: 18,
+    fontStyle: 'italic'
+  },
+  carName: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  carImage: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    marginBottom: 10,
+    resizeMode: 'contain'
   }
 })
